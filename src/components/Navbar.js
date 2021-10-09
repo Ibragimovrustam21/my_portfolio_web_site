6fac564e30b804b6f1952b0ac01220e3290678f7
Rustam21 <samsung122356@mail.ru>
2021-10-09 09:58:20 +0500

    'website'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { animateDown } from '../App'
 import '../css/navbar.css'
 import logo from '../logo/LogoMakr-2dsx6X.png'
 
-const Navbar = ({ history }) => {
+const Navbar = () => {
   const [path] = useState('')
 
   window.addEventListener('scroll', function () {
@@ -12,56 +12,62 @@ const Navbar = ({ history }) => {
     head.classList.toggle('sticky', window.scrollY > 0)
   })
 
-
   const navActive = () => {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".navbar-menu-group");
+    const blurback = document.querySelector('.blur-background')
 
+    blurback.classList.toggle("blur-back-show")
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("nav-active");
   }
 
 
+
+
   return (
-    <header className={path}>
-      <div className='navbar'>
-        <div className='navbar-logo-brand'>
-          <Link to='/'>
-            <img alt='' src={logo} />
-          </Link>
-        </div>
-        <div className='navbar-menu-group'>
-          <div className='navbar-menu-item'>
-            <div>
-              <h4 className='hoverLink' onClick={navActive} {...animateDown} data-aos-duration="600">
-                <Link to='/' ><span>01.</span> Главный</Link>
-              </h4>
-            </div>
-            <div>
-              <h4 className='hoverLink' onClick={navActive} {...animateDown}  data-aos-duration="800">
-                <Link to='/about' ><span>02.</span> Про меня</Link>
-              </h4>
-            </div>
-            <div>
-              <h4 className='hoverLink' onClick={navActive} {...animateDown} data-aos-duration="1000">
-                <Link to='/portfolio' className=''><span>03.</span> Портфолио</Link>
-              </h4>
-            </div>
-            <div>
-              <h4 className='hoverLink' onClick={navActive} {...animateDown} data-aos-duration="1200">
-                <Link to='/faqs' ><span>04.</span> Вопросы</Link>
-              </h4>
-            </div>
+    <>
+      <div className='blur-background'></div>
+      <header className={path}>
+        <div className='navbar'>
+          <div className='navbar-logo-brand'>
+            <Link to='/'>
+              <img alt='' src={logo} />
+            </Link>
           </div>
-          <a href='https://ibrag1movresume.netlify.app/' className='btn-link' {...animateDown} data-aos-duration="1400" >Резюме</a>
+          <div className='navbar-menu-group'>
+            <div className='navbar-menu-item'>
+              <div>
+                <h4 className='hoverLink' onClick={navActive} {...animateDown} data-aos-duration="600">
+                  <Link to='/' ><span>01.</span> Главный</Link>
+                </h4>
+              </div>
+              <div>
+                <h4 className='hoverLink' onClick={navActive} {...animateDown} data-aos-duration="800">
+                  <Link to='/about' ><span>02.</span> Про меня</Link>
+                </h4>
+              </div>
+              <div>
+                <h4 className='hoverLink' onClick={navActive} {...animateDown} data-aos-duration="1000">
+                  <Link to='/portfolio' className=''><span>03.</span> Портфолио</Link>
+                </h4>
+              </div>
+              <div>
+                <h4 className='hoverLink' onClick={navActive} {...animateDown} data-aos-duration="1200">
+                  <Link to='/faqs' ><span>04.</span> Вопросы</Link>
+                </h4>
+              </div>
+            </div>
+            <a href='https://ibrag1movresume.netlify.app/' className='btn-link' {...animateDown} data-aos-duration="1400" >Резюме</a>
+          </div>
+          <div className="hamburger" onClick={navActive}>
+            <span className="bar" />
+            <span className="bar" />
+            <span className="bar" />
+          </div>
         </div>
-        <div className="hamburger" onClick={navActive}>
-          <span className="bar" />
-          <span className="bar" />
-          <span className="bar" />
-        </div>
-      </div>
-    </header >
+      </header >
+    </>
   )
 }
 export default Navbar
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,7 +10,7 @@ export const HomePage = () => {
       <h1 {...animateUp} data-aos-duration="800">Ибрагимов Рустам</h1>
       <h2 {...animateUp} data-aos-duration="1000">Я разрабатываю адаптивные веб-сайты</h2>
       <p {...animateUp} data-aos-duration="1200">Я Фронтенд разработчик, специализирующийся на создании адаптивных веб-сайтов.В настоящее время работая над собой.Моя цель создать веб-сайты, которые принесут пользу людям.</p>
-      <Link className='btn-link' to='/portfolio'  {...animateUp} data-aos-duration="1400" >Портфолио</Link>
+      <Link className='btn-link' to='/portfolio'   {...animateUp} data-aos-duration="1400" >Портфолио</Link>
     </div>
   );
 };
