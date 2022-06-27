3c254461367e0008407283fd0eca4cc0f2fd73b8
Rustam21 <samsung122356@mail.ru>
2022-06-27 10:58:59 +0500

    'text'

--- src/components/MyProject/MyProject.jsx
+++ src/components/MyProject/MyProject.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { animateUp } from '../../settings/Animation';
 
 const MyProject = ({ title, text, technologies, url, urlGithub, animeTime, adaptive }) => (
-  <a target='_blank' href={url} {...animateUp} data-aos-duration={animeTime}>
+  <a target='_blank' rel="noreferrer" href={url} {...animateUp} data-aos-duration={animeTime}>
     <div className='project-item'>
       <div className='project-item-icon'>
         <div className='icon-folder'>
           <span className='far fa-folder' />
         </div>
         <div className='icon-source'>
-          <a target='_blank' href={urlGithub}><span className='fab fa-github hoverLink' /></a>
-          <a target='_blank' href={url}><span className='fas fa-external-link-alt hoverLink' /></a>
+          <a target='_blank' rel="noreferrer" href={urlGithub}><span className='fab fa-github hoverLink' /></a>
+          <a target='_blank' rel="noreferrer" href={url}><span className='fas fa-external-link-alt hoverLink' /></a>
         </div>
       </div>
       <div className='project-item-name'>
